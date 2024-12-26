8693d78c52a8d14bcaeccd44a73c0e685e01de8a
Watcharaponlee <watcharapon.lee@gmail.com>
2024-12-27 00:33:54 +0700

    Test_Deploy_V2

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   base: "/hny2025/",
-  build: {
-    outDir: "dist",
-  },
   plugins: [react()],
 });
